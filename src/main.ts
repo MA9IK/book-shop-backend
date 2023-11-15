7194c67774e50c3a0e758038e3bd10d9ba7b773a
Ivan <developerjsua@gmail.com>
2023-11-15 21:46:06 +0200

    feature / swagger connect

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule)
+	// there are swagger connecting
+	const config = new DocumentBuilder()
+		.setTitle('Book Shop')
+		.setDescription('There are api for my BookShop full-stack project')
+		.setVersion('1.0')
+		.addServer('http://localhost:4000/', 'Local environment')
+		// .addServer('https://staging.yourapi.com/', 'Staging')
+		// .addServer('https://production.yourapi.com/', 'Production')
+		.addTag('book')
+		.build()
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('api-docs', app, document)
+	await app.listen(4000)
 }
-bootstrap();
+bootstrap()
