8fd911f0dafd34249ae866eae3607ce401f730e3
Ivan <developerjsua@gmail.com>
2023-11-16 17:23:47 +0200

    feature / connect to mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({}),
+		MongooseModule.forRoot(
+			`mongodb+srv://Ivan:${process.env.PASSWORD_TO_DB}@cluster0.livtosq.mongodb.net/`
+		)
+	],
 	controllers: [],
 	providers: []
 })
