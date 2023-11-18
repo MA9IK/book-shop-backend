62ce7373deffa07712e7a0d2d09844ce1bc7ecb7
Ivan <developerjsua@gmail.com>
2023-11-18 20:19:39 +0200

    feature / implement jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,20 @@ import { ConfigModule } from '@nestjs/config'
 import { UserSchema } from './schemas/user.schema'
 import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
 	imports: [
-		ConfigModule.forRoot({}),
+		ConfigModule.forRoot({ isGlobal: true }),
 		MongooseModule.forRoot(
 			`mongodb+srv://Ivan:${process.env.PASSWORD_TO_DB}@cluster0.livtosq.mongodb.net/`
 		),
 		MongooseModule.forFeature([{ name: 'Users', schema: UserSchema }]),
+		JwtModule.register({
+			global: true,
+			secret: process.env.SECCRET_CODE_JWT,
+			signOptions: { expiresIn: '60s' }
+		}),
 		UsersModule,
 		AuthModule
 	],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,15 +3,20 @@ import {
 	NotFoundException,
 	UnauthorizedException
 } from '@nestjs/common'
-import { User } from 'src/schemas/user.schema'
 import { CreateUserDto } from 'src/users/dto/create-user.dto'
 import { IUser } from 'src/users/user.interface'
 import { UsersService } from 'src/users/users.service'
 import * as bcrypt from 'bcrypt'
+import { JwtService } from '@nestjs/jwt'
+import { JsonWebKeyInput } from 'crypto'
+
 @Injectable()
 export class AuthService {
-	constructor(private usersService: UsersService) {}
-	async signIn(email: string, pass: string): Promise<IUser> {
+	constructor(
+		private usersService: UsersService,
+		private jwtService: JwtService
+	) {}
+	async signIn(email: string, pass: string): Promise<{ access_token: string }> {
 		const user = await this.usersService.findOne(email)
 
 		if (!user) throw new NotFoundException('User not found')
@@ -19,7 +24,12 @@ export class AuthService {
 		const match = await bcrypt.compare(pass, password)
 
 		if (!match) throw new UnauthorizedException()
-		return user
+
+		const payload = { sub: user._id, username: user.username }
+
+		return {
+			access_token: await this.jwtService.signAsync(payload)
+		}
 	}
 
 	async signUp(CreateUserDto: CreateUserDto): Promise<IUser> {
